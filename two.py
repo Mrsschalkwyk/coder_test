33473725afafb2ea4d837bd04e40f1a3da33350c
Mrsschalkwyk <96808682+Mrsschalkwyk@users.noreply.github.com>
2022-05-11 20:39:31 +1000

    start_programing
    playing around with ideas

---
+++ two.py
@@ -0,0 +1,19 @@
+
+# user_input= input(f"check your number: ")
+# # word = input(f"please enter word: ")
+
+# fibonacci = [1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597]
+
+# if user_input in fibonacci:
+#     print("yes")
+# # else:
+# #     print("no")
+# a=input('check number')
+# b=[1,2,3,4]
+# if a in b:
+#     print('enter in code thats wanted')
+
+cars = [1,2,3]
+guess = int(input("colour: "))
+if guess in cars:
+  print ("success!")
